6692f59c40d44ed676eed0dcfe9565586f568e16
Artur <alferovnet@mail.ru>
2022-08-06 21:22:39 +0300

    add pods and created NetworkManager

--- CocktailRecipesApp/Services/NetworkManager.swift
+++ CocktailRecipesApp/Services/NetworkManager.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import Alamofire
+
+
+class NetworkManager {
+    
+    static  let shared = NetworkManager()
+    private let urlString = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+
+
+    private init() {}
+    
+    func fetchCocktails(completion : @escaping(Cocktails?) -> Void){
+        
+        AF.request(urlString).response { response in
+            if let error = response.error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = response.data else {return}
+            do {
+                let cocktail = try JSONDecoder().decode(Cocktails.self, from: data)
+                completion(cocktail)
+            }catch let error{
+                print(error.localizedDescription)
+            }
+        }
+}
+}
