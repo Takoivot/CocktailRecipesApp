f6c2916673678005dc9c64f7eef082f25b69b882
Artur <alferovnet@mail.ru>
2022-08-06 21:32:39 +0300

    Update ImageManager.swift
    add imageData func

--- CocktailRecipesApp/Services/ImageManager.swift
+++ CocktailRecipesApp/Services/ImageManager.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+class ImageManager {
+    static let shared = ImageManager()
+    private init() {}
+    
+    func fetchImage(from url: URL?) -> Date?{
+        guard let url = url else {return nil}
+        guard let imageData = try? Data(contentsOf: url) else {return nil}
+        return imageData
+    }
+}
